63987874f847c5c6f5ffdfd680f691eb0177a7fb
Devillzify <andresmunoz@paucasesnovescifp.cat>
2022-03-10 00:09:37 +0100

    database actualizado

--- Tema 7 joins y subquery/ex WORLD.sql
+++ Tema 7 joins y subquery/ex WORLD.sql
@@ -3,37 +3,88 @@ USE world;
 -- 1. Select identifier, name and population from only 10 records of cities.
 --    Order the data by population from the most populated to the least populated city
 
+select ID,Name,Population from city order by Population desc limit 10;
+
+select ID,Name,Population from city order by Population desc limit 10;
 -- 2. The same as in 1, but now only rows from 16 to 20.
+
+select ID,Name,Population from city order by Population desc limit 16,4;
 
 -- 3. Number of cities
 
+select  distinct count(*) from city;
+
 -- 4. Show name and population from the most populated city in the table.
+
+select * from city order by population desc limit 1;
 
 -- 5. Show name and population from the least populated city in the table.
 
+select name,population from city order by population limit 1;
+
 -- 6. Show name and population from the most and the least populated cities in the table.
+
+SELECT Name, Population from city
+WHERE Population = (SELECT MAX(Population) FROM city)
+OR Population = (SELECT MIN(Population) FROM city);
+
+select name,population from city 
+where population = (select max(population) from city);
+
+select name,population from city 
+where population = (select min(population) from city);
 
 -- 7. All cities (name and population) with a population above one million people.
 
+select name, population from city where population > 1000000;
+
 -- 8. Select all city names which begin with Kal.
+
+select name from city where name like "kal%";
 
 -- 9. Select all columns for one specific city, namely Bratislava.
 
+select * from city where name = "Bratislava";
+
 -- 10. Find out cities with a population between 670000 and 700000.
+
+select * from city where population between 670000 and 700000;
 
 -- 11. Returns the total number of people in the towns of the New York district.
 
+select sum(population),name from city where District = "New York";
+
 -- 12. Select every district from USA with its total number of people.
+
+select sum(population) from city where CountryCode = "USA";
 
 -- 13. Select all districts from USA which have population over 3 million people
 
+select * from city where CountryCode = "USA" group by district having population > 3000000;
+
+select district from city where CountryCode = "USA" and population > 3000000;
+
 -- 14. Show the number of countries and the average population per continent:
+
+select distinct Code,avg(population),count(Continent) from country group by Continent;
+
+select count(*), avg(population), continent from country group by continent;
 
 -- 15. Select all country names that doesn't have any city.
 
+select c.name from country c 
+left join city i on i.CountryCode = c.Code
+where i.CountryCode is null;
+
+select name, capital from country where Capital is null;
+
 -- 16. Select all country names with its number of cities (two ways: subquery and join).
 
+select name,count(city) from country;
+
 -- 17. Select all country names with its capital name (two ways: subquery and join).
+
+
 
 -- 18. Select repeated cities names and the number of repetition
 
